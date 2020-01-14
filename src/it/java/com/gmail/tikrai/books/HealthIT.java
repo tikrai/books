079a4f049db7a69735baae84b42579d20810f1e0
andrius <andrius@satalia.com>
2020-01-14 21:44:14 +0200

    Health indicator added

---
+++ src/it/java/com/gmail/tikrai/books/HealthIT.java
@@ -0,0 +1,25 @@
+package com.gmail.tikrai.books;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import com.gmail.tikrai.books.util.RestUtil.Endpoint;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+public class HealthIT extends IntegrationTestCase {
+
+  @Test
+  public void shouldShowHealth() {
+    Response response = given().get(Endpoint.HEALTH);
+
+    response.then()
+        .statusCode(HttpStatus.OK.value())
+        .contentType(ContentType.JSON)
+        .body("status", equalTo("UP"))
+        .body("components.db.status", equalTo("UP"))
+        .body("components.diskSpace.status", equalTo("UP"))
+        .body("components.ping.status", equalTo("UP"));
+  }
+}
--- src/main/java/com/gmail/tikrai/books/util/RestUtil.java
+++ src/main/java/com/gmail/tikrai/books/util/RestUtil.java
@@ -5,5 +5,6 @@ public class RestUtil {
   public static class Endpoint {
 
     public static final String BOOKS = "/books";
+    public static final String HEALTH = "/health";
   }
 }
