63c967a5d937d4e84af5d7b4bc5a3e992f4dc628
andrius <andrius@satalia.com>
2020-01-16 14:38:07 +0200

    Http errors include readable date instead of timestamp

--- src/main/java/com/gmail/tikrai/books/config/ObjectMapperConfiguration.java
+++ src/main/java/com/gmail/tikrai/books/config/ObjectMapperConfiguration.java
@@ -3,6 +3,7 @@ package com.gmail.tikrai.books.config;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +12,10 @@ public class ObjectMapperConfiguration {
 
   @Bean
   public ObjectMapper mapper() {
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.findAndRegisterModules();
-    mapper.setSerializationInclusion(Include.NON_ABSENT);
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    return mapper;
+    return new ObjectMapper()
+        .findAndRegisterModules()
+        .setSerializationInclusion(Include.NON_ABSENT)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
   }
 }
